6a3301d63ae4a9d3c9b290dd8faee0eba25bc206
claramtgs <clara.montagnes@polytechnique.edu>
2018-11-29 08:40:14 +0100

    idk

--- Engine/block.cpp
+++ Engine/block.cpp
@@ -2,13 +2,12 @@
 
 Block::Block(pair position, pair size, Block_type type, Block_state state, Block_texture texture) :
      Dead(position, size)
-{
-
-    this->position = position;
-    this->size = size;
-    this->state = state;
-    this->type = type;
-    this->texture = texture;
+{ //initialisation
+    this -> position = position;
+    this -> size = size;
+    this -> state = state;
+    this -> type = type;
+    this -> texture = texture;
 }
 
 void Block::hit_block() //Damage the block, from initial to damaged, from damaged to dead
