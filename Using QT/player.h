be83bcb44b9cd3d4ce220bc55666f4aa15b7bff0
SebastienLabbe <sebazore.bob@gmail.com>
2018-12-10 18:53:51 +0100

    cleaned version

---
+++ Using QT/player.h
@@ -0,0 +1,61 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <QGraphicsPixmapItem>
+#include <QGraphicsItem>
+#include <QObject>
+#include <QKeyEvent>
+#include "structures.h"
+
+class View;
+
+extern View* view;
+
+class Player : public QObject, public QGraphicsPixmapItem
+{
+
+    Q_OBJECT
+
+public:
+
+    Player(QGraphicsItem* parent = 0, int size = 36);
+
+
+    // Attributes
+    pair speedMax,speed;
+
+    double_pair world_boundaries;
+
+    bool pressedL{false}, pressedR{false};
+
+    int size{36}, M{10}, toMirror{4}, N{8}, direction{1}, block_size{18};
+
+    double count;
+
+    QPixmap animations[10][8];
+
+    QGraphicsRectItem* collision_range;
+
+
+    // Methods
+    void keyPressEvent(QKeyEvent * event);
+
+    void keyReleaseEvent(QKeyEvent * event);
+
+    void throwprojectile(int i);
+
+    void create_animation();
+
+    void create_collision_range();
+
+    bool collision_left(), collision_right(), collision_up(), collision_down();
+
+
+public slots:
+
+    // Slot for timer
+    void move();
+
+};
+
+#endif // PLAYER_H
