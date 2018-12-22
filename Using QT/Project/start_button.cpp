74e5a4e027a7666c8ae96551c923f94665f93cad
SebastienLabbe <sebazore.bob@gmail.com>
2018-12-22 10:37:21 +0100

    cleaning

--- Using QT/Project/start_button.cpp
+++
@@ -1,8 +0,0 @@
-#include "start_button.h"
-
-Start_button::Start_button(pair screen_size)
-{
-    setPixmap(QPixmap(":/Images/Menu/start_button.png"));
-
-    setPos(screen_size.x/2 - 3*18,-screen_size.y/2 + 1.5*18);
-}
