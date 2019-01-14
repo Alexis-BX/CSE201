d7255f0b838afe5fb73b33a75ba9dde1b6e4037d
claramtgs <clara.montagnes@polytechnique.edu>
2019-01-14 23:59:41 +0100

    new music and new images @hibabkd

--- Using QT/Project/you_win.cpp
+++ Using QT/Project/you_win.cpp
@@ -6,7 +6,7 @@ You_win::You_win(QGraphicsItem* parent):
     view->player->setX(0);
     view->player->playing = false;
     setPos(0,-view->screen_size.y+18);
-    QPixmap img(":/Images/Backgrounds/Main_background/you_win.png");
+    QPixmap img(":/Images/Backgrounds/Main_background/youwin.png");
     img = img.scaled(view->screen_size.x, view->screen_size.y);
     setPixmap(img);
     view->music->stop();
